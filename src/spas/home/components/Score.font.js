7ad865cf395dc66c708cfc47b4156445e7f4fa04
Bill Lee <github@evolutedesign.com>
2018-02-09 15:59:15 -0800

    Break key Components out into separeate files, separating not only implemenation code but also CSS and resources.

--- src/spas/home/components/Score.font.js
+++ src/spas/home/components/Score.font.js
@@ -1,8 +1,8 @@
-const BUNDLE_HELPER = require('../../utils')
+const BUNDLE_HELPER = require('../../../utils')
 
 module.exports = {
     files: [
-      './icons/beer.svg'
+      '../icons/beer.svg'
     ],
     fileName: BUNDLE_HELPER.path(__dirname) + '/fonts.[ext]'
   }
---
+++ src/spas/home/components/Score.js
@@ -0,0 +1,20 @@
+import React, {Component, PureComponent} from 'react'
+import './home.font'
+import './Score.css'
+
+/**
+ * Display the score, supplied to the tag.
+ *
+ * Attributes:
+ * - score - integer value that is the score.
+ */
+export default function Score(props) {
+	let score = props.score ? props.score : 0
+	let beers = new Array(score).fill(11)	// Create array so we can use map()
+
+	return (
+		<span className='score'>
+			{beers.map((i,idx) => (<span key={idx} className="icon icon-beer"/>))}
+		</span>
+	)
+} // Score component
