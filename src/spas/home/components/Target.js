f72204dcb7a301366b27dd8b2456c3fee38ca2fd
Bill Lee <github@evolutedesign.com>
2018-02-09 19:34:09 -0800

    Target.js:   - Randomize initial direction angle of movement.   - Fix portability issue in determining parent and element location--use     top/left rather than x/y.

--- src/spas/home/components/Target.js
+++ src/spas/home/components/Target.js
@@ -5,9 +5,9 @@ import './Target.css'
  * Timeout/interval handler that works as a promise.
  */
 export function timeout(delay) {
-	return new Promise( (resolve, reject) =>
-		setTimeout( resolve, delay )
-	)
+  return new Promise( (resolve, reject) =>
+    setTimeout( resolve, delay )
+  )
 } // timeout()
 
 /**
@@ -22,113 +22,122 @@ export function timeout(delay) {
  */
 export default class Target extends Component {
 
-	constructor(props) {
-		super(props)
+  constructor(props) {
+    super(props)
 
-		this.vector = {x:50, y:50}				// Movement vector
-    this.timeSlice = 125              // Animation timeslice, in milliseconds
-		this.state = {
-			show: false											// Has target been mounted in React?
-		}
+    const too_narrow = 15					// Avoid angles that are to close to vert/horz
+    let angle=0
+                                  // Get random angle that's not too narrow
+    while (angle % 90 < too_narrow | angle % 90 > 90-too_narrow)
+      angle = Math.random() * (360-too_narrow) + too_narrow;
+    angle *= Math.PI / 180				// Convert to radians
 
-		this.handleClick = this.handleClick.bind(this)
-		this.move = this.move.bind(this)
-	} // constructor()
+    this.vector = {								// Movement vector
+      x: Math.cos(angle) *  50,		// Multiply "angle" by magnitude
+      y: Math.sin(angle) * -50		// Vertical coordinates are reversed
+    }
+    this.timeSlice = 125					// Animation timeslice, in milliseconds
+    this.state = {
+      show: false									// Has target been mounted in React?
+    }
 
-	/**
-	 * Since the initial position of the target is relative to the play area, we
-	 * have to wait until this component is mounted before we can determine where
-	 * to place it.
-	 */
-	componentDidMount() {
-		if (!this.state.show) {
-			const prect=this.parentNode.getBoundingClientRect()
-			const crect=this.node.firstChild.getBoundingClientRect()
-			this.setState({
-				show: true,
-				x: prect.x + prect.width*Math.random() - crect.width/2,
-				y: prect.y + prect.height*Math.random() - crect.height/2
-			})
-		}
-		if (this.props.move !== 0) {
-			this.moveCount = Number.isInteger(this.props.move) ? this.props.move : -1
-			this.move()
-		}
-	} // componentDidMount()
+    this.handleClick = this.handleClick.bind(this)
+    this.move = this.move.bind(this)
+  } // constructor()
 
-	/**
-	 * Move the "target" within the play area (the parent area).
-	 */
-	move() {
+  /**
+   * Since the initial position of the target is relative to the play area, we
+   * have to wait until this component is mounted before we can determine where
+   * to place it.
+   */
+  componentDidMount() {
+    if (!this.state.show) {
+      const prect=this.parentNode.getBoundingClientRect()
+      const crect=this.node.firstChild.getBoundingClientRect()
+      this.setState({						// Turn on target and init its location randomly
+        show: true,
+        x: prect.left + prect.width *Math.random() - crect.width/2,
+        y: prect.top  + prect.height*Math.random() - crect.height/2
+      })
+      if (this.props.move !== 0) {
+        this.moveCount = Number.isInteger(this.props.move) ? this.props.move : -1
+        this.move()
+      }
+    }
+  } // componentDidMount()
+
+  /**
+   * Move the "target" within the play area (the parent area).
+   */
+  move() {
 //		console.log('move',this.vector, this.state);
-		if ( ! this.parentNode || ! this.node.firstChild )
-			return;
-		const prect=this.parentNode.getBoundingClientRect()
-		const crect=this.node.firstChild.getBoundingClientRect()
+    if ( ! this.parentNode || ! this.node.firstChild )
+      return;
+    const prect=this.parentNode.getBoundingClientRect()
+    const crect=this.node.firstChild.getBoundingClientRect()
 
-		this.setState(state => {
-//			console.log('child',crect);
-//			console.log('parent',prect);
-													// Move the target
-			state.x += this.vector.x
-			state.y += this.vector.y
+    this.setState(state => {
+      // Move the target
+      state.x += this.vector.x
+      state.y += this.vector.y
 
-			// Check to see whether we moved (a little) out of bounds and
-			// limit the movement, if it has. Then adjust the movement direction
-			// vector so the target doesn't keep trying to move outside the
-			// boundary.
-			if (state.x < prect.x) {
-				state.x = prect.x
-				this.vector.x = -this.vector.x
-			}
-			if (state.y < prect.y) {
-				state.y = prect.y
-				this.vector.y = -this.vector.y
-			}
+      // Check to see whether we moved (a little) out of bounds and
+      // limit the movement, if it has. Then adjust the movement direction
+      // vector so the target doesn't keep trying to move outside the
+      // boundary.
+      if (state.x < prect.left) {
+        state.x = prect.left
+        this.vector.x = -this.vector.x
+      }
+      if (state.y < prect.top) {
+        state.y = prect.top
+        this.vector.y = -this.vector.y
+      }
 
-			if (state.x+crect.width/2 > prect.x+prect.width) {
-				state.x = prect.x+prect.width - crect.width/2
-				this.vector.x = -this.vector.x
-			}
-			if (state.y+crect.height/2 > prect.y+prect.height) {
-				state.y = prect.y+prect.height - crect.height/2
-				this.vector.y = -this.vector.y
-			}
-			return state
-		})
-		if (this.moveCount--)				// In case moves are limited
-			timeout(this.timeSlice).then(this.move)	// Move, again, after a short pause.
-	}
+      if (state.x+crect.width/2 > prect.left+prect.width) {
+        state.x = prect.left+prect.width - crect.width/2
+        this.vector.x = -this.vector.x
+      }
+      if (state.y+crect.height/2 > prect.top+prect.height) {
+        state.y = prect.top+prect.height - crect.height/2
+        this.vector.y = -this.vector.y
+      }
+      return state
+    })
+    if (this.moveCount--)				// In case moves are limited
+      timeout(this.timeSlice).then(this.move)	// Move, again, after a short pause.
+  }
 
-	componentWillUnmount() {
-		this.moveCount = 0;
-	}
+  componentWillUnmount() {
+    this.moveCount = 0;
+  }
 
-	/**
-	 * Propagate "hits" to registered onClick function
-	 */
-	handleClick(e) {
-													// If listener is registered, call it.
-		if (typeof this.props.onClick == 'function') {
-			this.props.onClick.apply(null,arguments)
-//			e.preventDefault()
-			e.stopPropagation()
-		}
-	} // handleClick()
+  /**
+   * Propagate "hits" to registered onClick function
+   */
+  handleClick(e) {
+                          // If listener is registered, call it.
+    if (typeof this.props.onClick == 'function') {
+      this.props.onClick.apply(null,arguments)
+      // this.setState({reverse: !this.state.reverse})
+      e.stopPropagation()
+    }
+  } // handleClick()
 
-	render() {
-		return (
-			<span ref={target => (this.node= target,this.parentNode = (target ? target.parentElement : null)) }>
-			<GithubKitty
-				className='target'
-				style={{
+  render() {
+    return (
+      <span ref={target => (this.node= target,this.parentNode = (target ? target.parentElement : null)) }>
+      <GithubKitty
+        className='target'
+        style={{
+          animationDirection: this.state.reverse ? 'reverse' : 'normal',
           transition: this.timeSlice/1000+'s linear',
-					visibility: this.state.show ? 'visible' : 'hidden',
-					top: this.state.y,
-					left: this.state.x
-				}}
-				onClick={this.handleClick} />
-			</span>
-		)
-	} // render()
+          visibility: this.state.show ? 'visible' : 'hidden',
+          top: this.state.y,
+          left: this.state.x
+        }}
+        onClick={this.handleClick} />
+      </span>
+    )
+  } // render()
 } // class Target
--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -19,105 +19,105 @@ import Score from './components/Score'
  * - <WinnerBanner> - Manages content displayed to a winner.
  */
 class GameBoard extends Component {
-	constructor(props) {
-		super(props)
-									// What score is needed to win? Dft: 10
-		this.win = props.win ? props.win : 10
-		this.state = {
-			score: 0,			// Game score
-			winner: false		// Did the player win?
-		}
-		this.onTargetClick = this.onTargetClick.bind(this)
-		this.onMissedClick = this.onMissedClick.bind(this)
-	}
+  constructor(props) {
+    super(props)
+                      // What score is needed to win? Dft: 10
+    this.win = props.win ? props.win : 10
+    this.state = {
+      score: 0,       // Game score
+      winner: false   // Did the player win?
+    }
+    this.onTargetClick = this.onTargetClick.bind(this)
+    this.onMissedClick = this.onMissedClick.bind(this)
+  }
 
-	/**
-	 * Detected a click in the board's area, so it missed! Deduct a point
-	 * from the score.
-	 */
-	onMissedClick(e) {
-//		console.log('missed click',e, e.persist(),e.nativeEvent);
-		if (!this.state.winner) {
-			e.persist()
-			this.setState(state => (state.score=Math.max(state.score-1, 0),
-											state.missed = {x:e.nativeEvent.x, y:e.nativeEvent.y},
-		 									state))
-//			timeout(1000).then(this.setState(state => (delete state.missed, state)))
-	 	}
-	} // onMissedClick()
+  /**
+   * Detected a click in the board's area, so it missed! Deduct a point
+   * from the score.
+   */
+  onMissedClick(e) {
+//  console.log('missed click',e, e.persist(),e.nativeEvent);
+    if (!this.state.winner) {
+      e.persist()
+      this.setState(
+        state => (state.score=Math.max(state.score-1, 0),
+                  state.missed = {x:e.nativeEvent.x, y:e.nativeEvent.y},
+                  state))
+     }
+  } // onMissedClick()
 
-	/**
-	 * Handle "hits" reported by the target object. Increment the score
-	 */
-	onTargetClick(e) {
-		this.setState(
-			state => (state.score=Math.min(state.score+1, this.win),
-						 delete state.missed,
-		 			    state.winner=state.score >=this.win,
-		 			 	 state)
-		)
-	} // onTargetClick()
+  /**
+   * Handle "hits" reported by the target object. Increment the score
+   */
+  onTargetClick(e) {
+    this.setState(
+      state => (state.score=Math.min(state.score+1, this.win),
+                delete state.missed,
+                state.winner=state.score >=this.win,
+                state)
+    )
+  } // onTargetClick()
 
-	/**
-	 * Render the game board
-	 */
-	render() {
-		if (this.state.winner) {
-			return (
-				<div className='board'>
-					<WinnerBanner>You won! You're the winner!! You won the game! You killed all the pigs and poked all the cats&hellip; you can rest easy that you are the king of the pigs and one cool cat. Should anyone ever question you, just be sure to tell them that no one handles pigs and cats as will as you do, because, you are THE winner!!
-				 </WinnerBanner>
-				</div>
-			)
-		}
-		else {
-			const title = this.props.children
-						   ? (<span className='title'>{this.props.children}</span>)
-							: ''
+  /**
+   * Render the game board
+   */
+  render() {
+    if (this.state.winner) {
+      return (
+        <div className='board'>
+          <WinnerBanner>You won! You're the winner!! You won the game! You killed all the pigs and poked all the cats&hellip; you can rest easy that you are the king of the pigs and one cool cat. Should anyone ever question you, just be sure to tell them that no one handles pigs and cats as will as you do, because, you are THE winner!!
+         </WinnerBanner>
+        </div>
+      )
+    }
+    else {
+      const title = this.props.children
+               ? (<span className='title'>{this.props.children}</span>)
+              : ''
 
-			let missed=''
-			if (this.state.missed) {
-//				missed=(<Missed x={this.state.missed.x} y={this.state.missed.y} />)
-				missed=(<div
+      let missed=''
+      if (this.state.missed) {
+//        missed=(<Missed x={this.state.missed.x} y={this.state.missed.y} />)
+        missed=(<div
                   className='missed'
                   style={{left:this.state.missed.x, top:this.state.missed.y}}>
                     Missed!
                 </div>)
-			}
-			return (
-				<div className='board' onClick={this.onMissedClick}>
-					{title}
-					<Score score={this.state.score}/>
-					<Target move={-1} onClick={this.onTargetClick}/>
-					{missed}
-				</div>
-			)
-		}
-	} // render()
+      }
+      return (
+        <div className='board' onClick={this.onMissedClick}>
+          {title}
+          <Score score={this.state.score}/>
+          <Target move={-1} onClick={this.onTargetClick}/>
+          {missed}
+        </div>
+      )
+    }
+  } // render()
 } // class GameBoard
 
 class Missed extends PureComponent {
 
-	constructor(props) {
-		super(props)
-		console.log('<Missed>', this, props)
-	}
+  constructor(props) {
+    super(props)
+    console.log('<Missed>', this, props)
+  }
 
-	componentWillReceiveProps(props) {
-		console.log('componentWillReceiveProps', this.props, props, ReactDOM.findDOMNode(this));
-		let me = ReactDOM.findDOMNode(this)
-		console.log('styles',me.style, me.style.opacity);
-//		console.log('attributes',me.attributes);
-		me.style.animation = 'none'
-		console.log('styles',me.style, me.style.opacity);
-	}
+  componentWillReceiveProps(props) {
+    console.log('componentWillReceiveProps', this.props, props, ReactDOM.findDOMNode(this));
+    let me = ReactDOM.findDOMNode(this)
+    console.log('styles',me.style, me.style.opacity);
+//    console.log('attributes',me.attributes);
+    me.style.animation = 'none'
+    console.log('styles',me.style, me.style.opacity);
+  }
 
-	render() {
-		console.log('render', this);
-		return (
-			<div className='missed' style={{left:this.props.x, top:this.props.y}}>Missed!!</div>
-		)
-	}
+  render() {
+    console.log('render', this);
+    return (
+      <div className='missed' style={{left:this.props.x, top:this.props.y}}>Missed!!</div>
+    )
+  }
 }
 
 /**
@@ -125,10 +125,10 @@ class Missed extends PureComponent {
  * component-tags will be used for display.
  */
 function WinnerBanner(props) {
-	console.log(props);
-	return (
-		<div className='winningBanner'>{props.children} {props.children}</div>
-	)
+  console.log(props);
+  return (
+    <div className='winningBanner'>{props.children} {props.children}</div>
+  )
 } // WinnerBanner component
 
 /**
