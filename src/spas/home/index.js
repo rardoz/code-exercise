14a3a5a2119f2143703a0c08260671461d718e9e
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-15 17:57:26 -0800

    pass GameCanvas a Game instance

--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import GameCanvas from './components/GameCanvas/GameCanvas';
+import { render } from 'react-dom';
+import GameCanvas from './container/GameCanvas/GameCanvas';
 import './styles.scss';
 import './home.font';
+import { GameBuilder } from './container/GameCanvas/GameBuilder';
 
-ReactDOM.render(<GameCanvas />, document.getElementById('react-spa'));
+// DEBUG: Game (w/ default options)
+const game = new GameBuilder().build();
+
+render(
+  <GameCanvas {...game} />,
+  document.getElementById('react-spa')
+);
