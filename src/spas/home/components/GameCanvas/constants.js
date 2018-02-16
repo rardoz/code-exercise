583886f38f8a02734655170d2bf3ddc8041311ff
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-15 16:48:19 -0800

    remove unused constants for initial X and Y and tweak other values

--- src/spas/home/components/GameCanvas/constants.js
+++ src/spas/home/components/GameCanvas/constants.js
@@ -2,16 +2,14 @@ export const SCORE_INITIAL = 0;
 export const SCORE_WINNING = 10;
 export const SCORE_INCREMENT = 1;
 
-export const SPEED_INITIAL = 1;
-export const SPEED_INCREMENT = 0.5;
+export const SPEED_INITIAL = 5;
+export const SPEED_INCREMENT = 0.75;
 
-export const BOARD_WIDTH = '95%';
-export const BOARD_HEIGHT = '95%';
+export const BOARD_WIDTH = '80%';
+export const BOARD_HEIGHT = '80%';
 
 export const ICON_WIDTH = 50;
 export const ICON_HEIGHT = 50;
-export const ICON_X_INITIAL = 50;
-export const ICON_Y_INITIAL = 50;
 
-export const DRAW_RATE = 35;
+export const DRAW_RATE = 30;
 
