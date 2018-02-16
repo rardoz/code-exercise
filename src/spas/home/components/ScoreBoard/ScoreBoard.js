b51520da1932324df64613b0a5b59a31b46ff3de
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-15 21:02:02 -0800

    show all score board with points more opaque

--- src/spas/home/components/ScoreBoard/ScoreBoard.js
+++ src/spas/home/components/ScoreBoard/ScoreBoard.js
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const ScoreBoard = ({score}) => Array.from({length: score}, (_, i) => <span key={i} className="icon icon-beer" />);
+const ScoreBoard = ({score, total, grid}) => {
+
+  const isActive = (i) => i < score ? 'active' : '';
+
+  return (
+    <section style={grid}>
+      {Array.from({length: total}, (_, i) => <span key={i} className={`score icon icon-beer ${isActive(i)}`} />)}
+    </section>
+  );
+};
 
 export default ScoreBoard;
