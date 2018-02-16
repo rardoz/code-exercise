7b1da0ebce519a70034e86a830e4a7f00700b924
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-15 21:03:16 -0800

    add animation and explicit onClick to GithubKitty

--- src/spas/home/components/GithubKitty/GithubKitty.js
+++ src/spas/home/components/GithubKitty/GithubKitty.js
@@ -2,21 +2,26 @@ import React from 'react';
 import Icon from '../../github.svg';
 // import Hidden from '../Hidden/Hidden';
 
-const GithubKitty = ({x, y, w, h, hidden, ...props}) => {
+const defaultIconStyles = {
+  animation: 'spin infinite 20s linear',
+  display: 'block',
+  position: 'absolute',
+  cursor: 'pointer',
+};
+
+const GithubKitty = ({x, y, w, h, s, hidden, onClick}) => {
 
   if (hidden) return null;
 
   const style = {
-    display: 'block',
-    position: 'absolute',
-    cursor: 'pointer',
     width: w,
     height: h,
+    ...defaultIconStyles
   };
 
   return (
-    <div style={{...style, left: x, top: y}} {...props}>
-      <Icon width={w} height={h} />
+    <div style={{...style, left: x, top: y}}>
+      <Icon width={w} height={h} onClick={onClick} />
     </div>
   );
 
