88cf548c9855c0e8284feef0939d6d5624742add
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-15 21:04:31 -0800

    move Game instance inside of root component

--- src/spas/home/index.js
+++ src/spas/home/index.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import { render } from 'react-dom';
-import GameCanvas from './container/GameCanvas/GameCanvas';
 import './styles.scss';
 import './home.font';
-import { GameBuilder } from './container/GameCanvas/GameBuilder';
-
-// DEBUG: Game (w/ default options)
-const game = new GameBuilder().build();
+import GameLayout from './container/Game/GameLayout';
 
 render(
-  <GameCanvas {...game} />,
+  <GameLayout />,
   document.getElementById('react-spa')
 );
