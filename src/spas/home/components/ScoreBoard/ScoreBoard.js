69a615e3d9f7bbc406043fd2d3ef4c567918f2ef
Travis Clarke <travis.m.clarke@gmail.com>
2018-02-16 17:52:47 -0800

    add 'Score' text to ScoreBoard

--- src/spas/home/components/ScoreBoard/ScoreBoard.js
+++ src/spas/home/components/ScoreBoard/ScoreBoard.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ScoreBoard = ({score, total, style}) => {
   return (
     <section style={style}>
-      {Array.from({length: total}, (_, i) => <span key={i} className={`score icon icon-beer ${i < score ? 'active' : ''}`} />)}
+      Score: {Array.from({length: total}, (_, i) => <span key={i} className={`score icon icon-beer ${i < score ? 'active' : ''}`} />)}
     </section>
   );
 };
